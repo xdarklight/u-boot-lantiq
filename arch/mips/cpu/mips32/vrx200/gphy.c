57d76fffa06c9f11e582a3de84e1326bbf3ee81d
Daniel Schwierzeck <daniel.schwierzeck@gmail.com>
2014-02-09 01:45:51 +0100

    MIPS: vrx200: switch to firmware loader
    Signed-off-by: Daniel Schwierzeck <daniel.schwierzeck@gmail.com>

--- arch/mips/cpu/mips32/vrx200/gphy.c
+++ arch/mips/cpu/mips32/vrx200/gphy.c
@@ -5,54 +5,41 @@
  */
 
 #include <common.h>
+#include <firmware.h>
 #include <asm/lantiq/io.h>
 #include <asm/arch/soc.h>
 #include <asm/arch/gphy.h>
 
-static inline void ltq_gphy_copy(const void *fw_start, const void *fw_end,
-				ulong dst_addr)
+static void ltq_fw_load(const char *name, ulong dst_addr)
 {
-	const ulong fw_len = (ulong) fw_end - (ulong) fw_start;
 	const ulong addr = CKSEG1ADDR(dst_addr);
+	const struct firmware *fw;
 
-	debug("ltq_gphy_copy: addr %08lx, fw_start %p, fw_end %p\n",
-		addr, fw_start, fw_end);
+	int err;
 
-	memcpy((void *) addr, fw_start, fw_len);
+	err = request_firmware(&fw, "lantiq/vrx200_phy11g_a1x.bin");
+	if (!err)
+		memcpy((void *) addr, fw->data, fw->size);
+
+	release_firmware(fw);
 }
 
 void ltq_gphy_phy11g_a1x_load(ulong addr)
 {
-	extern ulong __ltq_fw_phy11g_a1x_start;
-	extern ulong __ltq_fw_phy11g_a1x_end;
-
-	ltq_gphy_copy(&__ltq_fw_phy11g_a1x_start, &__ltq_fw_phy11g_a1x_end,
-			addr);
+	ltq_fw_load("lantiq/vrx200_phy11g_a1x.bin", addr);
 }
 
 void ltq_gphy_phy11g_a2x_load(ulong addr)
 {
-	extern ulong __ltq_fw_phy11g_a2x_start;
-	extern ulong __ltq_fw_phy11g_a2x_end;
-
-	ltq_gphy_copy(&__ltq_fw_phy11g_a2x_start, &__ltq_fw_phy11g_a2x_end,
-			addr);
+	ltq_fw_load("lantiq/vrx200_phy11g_a2x.bin", addr);
 }
 
 void ltq_gphy_phy22f_a1x_load(ulong addr)
 {
-	extern ulong __ltq_fw_phy22f_a1x_start;
-	extern ulong __ltq_fw_phy22f_a1x_end;
-
-	ltq_gphy_copy(&__ltq_fw_phy22f_a1x_start, &__ltq_fw_phy22f_a1x_end,
-			addr);
+	ltq_fw_load("lantiq/vrx200_phy22f_a1x.bin", addr);
 }
 
 void ltq_gphy_phy22f_a2x_load(ulong addr)
 {
-	extern ulong __ltq_fw_phy22f_a2x_start;
-	extern ulong __ltq_fw_phy22f_a2x_end;
-
-	ltq_gphy_copy(&__ltq_fw_phy22f_a2x_start, &__ltq_fw_phy22f_a2x_end,
-			addr);
+	ltq_fw_load("lantiq/vrx200_phy22f_a2x.bin", addr);
 }
