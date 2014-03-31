1afdaa0a12d02e78ac9f54be09f38cefac0e1045
Daniel Schwierzeck <daniel.schwierzeck@gmail.com>
2014-03-31 22:32:02 +0200

    MIPS: easy50712: enable mtdparts
    Signed-off-by: Daniel Schwierzeck <daniel.schwierzeck@gmail.com>

--- include/configs/easy50712.h
+++ include/configs/easy50712.h
@@ -41,20 +41,30 @@
 #if defined(CONFIG_SYS_BOOT_NOR)
 #define CONFIG_ENV_IS_IN_FLASH
 #define CONFIG_ENV_OVERWRITE
-#define CONFIG_ENV_OFFSET		(256 * 1024)
+#define CONFIG_ENV_OFFSET		(512 * 1024)
 #define CONFIG_ENV_SECT_SIZE		(64 * 1024)
+#define MTDPARTS_DEFAULT		\
+	"mtdparts=ltq_nor:512k(uboot_fix),64k(uboot_cfg)"
 #elif defined(CONFIG_SYS_BOOT_NORSPL)
 #define CONFIG_ENV_IS_IN_FLASH
 #define CONFIG_ENV_OVERWRITE
-#define CONFIG_ENV_OFFSET		(128 * 1024)
+#define CONFIG_ENV_OFFSET		(256 * 1024)
 #define CONFIG_ENV_SECT_SIZE		(64 * 1024)
+#define MTDPARTS_DEFAULT		\
+	"mtdparts=ltq_nor:256k(uboot_fix),64k(uboot_cfg)"
 #else
 #define CONFIG_ENV_IS_NOWHERE
+#define MTDPARTS_DEFAULT		"mtdparts="
 #endif
 
 #define CONFIG_ENV_SIZE			(8 * 1024)
 
 #define CONFIG_LOADADDR			CONFIG_SYS_LOAD_ADDR
+
+/* MTD/UBI */
+#define CONFIG_MTD_PARTITIONS
+#define CONFIG_FLASH_CFI_MTD
+#define MTDIDS_DEFAULT			"nor0=ltq_nor,nand0=nand-xway"
 
 /* Console */
 #define CONFIG_LTQ_ADVANCED_CONSOLE
