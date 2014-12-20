e35818f9f5012f9a995a515b715eb26b9bcd6c69
Daniel Schwierzeck <daniel.schwierzeck@gmail.com>
2014-12-20 19:58:22 +0100

    MIPS: add support for Lantiq XWAY XRX200 SoC family
    Signed-off-by: Daniel Schwierzeck <daniel.schwierzeck@gmail.com>

---
+++ arch/mips/include/asm/arch-xrx200/soc.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2014 Daniel Schwierzeck, daniel.schwierzeck@gmail.com
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __ARCH_XRX200_SOC_H
+#define __ARCH_XRX200_SOC_H
+
+#endif /* __ARCH_XRX200_SOC_H */
