d46a6dab402fcefa840d0351c3cd5c837e3dc69c
Daniel Schwierzeck <daniel.schwierzeck@gmail.com>
2014-12-20 19:58:23 +0100

    MIPS: add support for Lantiq XWAY Danube SoC family
    Signed-off-by: Daniel Schwierzeck <daniel.schwierzeck@gmail.com>

---
+++ arch/mips/arch-lantiq/platdevice-danube.c
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2014 Daniel Schwierzeck, daniel.schwierzeck@gmail.com
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <dm/platdata.h>
+#include <dm/platform_data/serial_lantiq.h>
+
+static const struct lantiq_serial_platform_data uart1_dev = {
+	.base = 0x1E100C00,
+	.uart_clk = 166666667,
+};
+
+U_BOOT_DEVICE(serial1) = {
+	.name = "lantiq-uart",
+	.platdata = &uart1_dev,
+};
---
+++ arch/mips/include/asm/arch-danube/soc.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2014 Daniel Schwierzeck, daniel.schwierzeck@gmail.com
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __ARCH_DANUBE_SOC_H
+#define __ARCH_DANUBE_SOC_H
+
+#endif /* __ARCH_DANUBE_SOC_H */
