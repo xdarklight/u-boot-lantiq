940af1c392fe88685b4c452f2995519d8a88c03d
Daniel Schwierzeck <daniel.schwierzeck@gmail.com>
2014-12-20 19:58:23 +0100

    MIPS: add Lantiq EASY80920 board
    Signed-off-by: Daniel Schwierzeck <daniel.schwierzeck@gmail.com>

---
+++ include/configs/easy80920.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2014 Daniel Schwierzeck, daniel.schwierzeck@gmail.com
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <asm/arch-lantiq/config.h>
+#include <asm/arch/config.h>
+
+#define CONFIG_SYS_NO_FLASH
+
+#define CONFIG_ENV_IS_NOWHERE
+#define CONFIG_ENV_SIZE			(8 * 1024)
